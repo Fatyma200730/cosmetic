ee93974d68d96701b56db33df649264d744177b2
Fatima <fatimazin980@gmail.com>
2025-03-15 15:12:16 +0000

    Ajout du fichier Register.jsx

--- makeup-store-frontend/src/Pages/Register.jsx
+++ makeup-store-frontend/src/Pages/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:8000/register', { name, email, password });
+      await axios.post('http://localhost:8000/api/register', { name, email, password });
       navigate('/login');
     } catch (error) {
       console.error('Registration failed', error);
